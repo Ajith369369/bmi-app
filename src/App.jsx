db5456c985d038433e4637f92c8f039cc23a77d7
Ajith K. S. <ajithks98086@gmail.com>
2024-07-07 14:08:08 +0530

    Add .flex-column

--- src/App.jsx
+++ src/App.jsx
@@ -25,8 +25,8 @@ const App = () => {
       <div className="container-fluid w-100">
         <div className="row">
           <div className="col-sm-0 col-md-1 col-lg-1 col-xl-1 col-xxl-1"></div>
-          <div className="col-sm-12 col-md-10 col-lg-10 col-xl-10 col-xxl-10 d-flex justify-content-center align-items-center">
-            <div className="App">
+          <div className="col-sm-12 col-md-10 col-lg-10 col-xl-10 col-xxl-10 d-flex flex-column justify-content-center align-items-center">
+            <div className="App d-flex flex-column justify-content-center align-items-center">
               <h1>BMI Calculator</h1>
 
               {/* Props Sent to "BMIForm" */}
