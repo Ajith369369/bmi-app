14675d47d35b5f1c9a91fcbe4ae976d26cd1144f
Ajith K. S. <ajithks98086@gmail.com>
2024-07-08 09:35:30 +0530

    Make background color change.

--- src/components/BMIResult.jsx
+++ src/components/BMIResult.jsx
@@ -1,18 +1,29 @@
 // Receives the calculated BMI as a prop (`bmi`) and displays it.
 // This component displays the BMI result and its category.
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import "../BMIResult.css";
 
 // Receiving Props
 const BMIResult = ({ bmi }) => {
-  let category = '';
-  if (bmi < 18.5) category = 'Underweight';
-  else if (bmi < 25) category = 'Normal weight';
-  else if (bmi < 30) category = 'Overweight';
-  else category = 'Obesity';
+  let category = "";
+  let backgroundColor = "";
+
+  if (bmi < 18.5) {
+    category = "Underweight";
+    backgroundColor = "blue";
+  } else if (bmi < 25) {
+    category = "Normal weight";
+    backgroundColor = "green";
+  } else if (bmi < 30) {
+    category = "Overweight";
+    backgroundColor = "orange";
+  } else {
+    category = "Obesity";
+    backgroundColor = "red";
+  }
 
   return (
-    <div className="bmi-result d-flex flex-column justify-content-center align-items-center">
+    <div className="bmi-result d-flex flex-column justify-content-center align-items-center" style={{ backgroundColor, color: 'white', padding: '10px', borderRadius: '5px' }}>
       <h2>Your BMI: {bmi.toFixed(2)}</h2>
       <p>Category: {category}</p>
     </div>
@@ -21,7 +32,7 @@ const BMIResult = ({ bmi }) => {
 
 // PropTypes validation
 BMIResult.propTypes = {
-  bmi: PropTypes.number.isRequired
+  bmi: PropTypes.number.isRequired,
 };
 
 export default BMIResult;
